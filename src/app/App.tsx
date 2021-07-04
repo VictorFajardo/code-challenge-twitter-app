3b095de163e1288f93c4d05997f89976ebc00d2e
Victor Fajardo <fajardo.de.leon@gmail.com>
2021-07-04 10:42:23 -0400

    styles update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,16 +2,20 @@ import Search from './components/Search'
 import List from './components/List'
 import Filter from './components/Filter'
 import Header from './components/Header';
+import Test from './Test'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      {/* <Test /> */}
       <Header />
-      <Search />
-      <Filter />
-      <List />
-    </div>
+      <main>
+        <Search />
+        <Filter />
+        <List />
+      </main>
+    </>
   );
 }
 

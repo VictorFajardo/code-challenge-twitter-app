6213ec1ba76ce31e51cf6240bd1058daa6f3eadc
VictorFajardo <fajardo.de.leon@gmail.com>
2021-07-05 10:17:55 -0400

    proxy api selected for production

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,8 +1,8 @@
 const config = {
   // Production
-  // base_url: 'https://code-challenge-proxy-server.herokuapp.com/search',
+  base_url: 'https://code-challenge-proxy-server.herokuapp.com/search',
   // Local
-  base_url: 'http://localhost:5000/search',
+  // base_url: 'http://localhost:5000/search',
   count: 5,
   result_type: 'popular',
   tweet_mode: 'extended',

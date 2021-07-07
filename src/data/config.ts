2a4f575df9a4d6b2304c7692c172a279a0b21336
Victor Fajardo <fajardo.de.leon@gmail.com>
2021-07-06 23:50:12 -0400

    adding api endpoint for production

--- src/data/config.ts
+++ src/data/config.ts
@@ -2,7 +2,7 @@ const config = {
   // * Local
   //  base_url: 'http://localhost:5000',
   // * Production
-   base_url: 'https://code-challenge-proxy-server.herokuapp.com/search',
+   base_url: 'https://code-challenge-proxy-server.herokuapp.com',
   count: 5,
   result_type: 'popular',
   tweet_mode: 'extended',
